1fc52f6b262822d897b26067e76136dff0cb2491
Ahkam Naseek <mohamedahkam.16@cse.mrt.ac.lk>
2021-04-14 19:36:07 +0530

    minor changes

--- file_transfering/ReceiveData.java
+++ file_transfering/ReceiveData.java
@@ -5,48 +5,43 @@ import java.net.Socket;
 
 public class ReceiveData implements Runnable {
     private Socket srvr_sckt;
-    private BufferedReader in = null;
+    private BufferedReader input = null;
     private String fileName;
 
     public ReceiveData(Socket socket, String fileName) {
-        this.srvr_sckt = socket;
         this.fileName = fileName;
+        this.srvr_sckt = socket;
     }
 
     @Override
     public void run() {
         try {
-            in = new BufferedReader(new InputStreamReader(srvr_sckt.getInputStream()));
-            DataOutputStream dOut = new DataOutputStream(srvr_sckt.getOutputStream());
-            dOut.writeUTF(fileName);
-            dOut.flush();
-
-            String canonicalPath = new File(".").getCanonicalPath();
-            System.out.println("Current directory path using canonical path method: " + canonicalPath);
-
+            input = new BufferedReader(new InputStreamReader(srvr_sckt.getInputStream()));
+            DataOutputStream data_out_str = new DataOutputStream(srvr_sckt.getOutputStream());
+            data_out_str.writeUTF(fileName);
+            data_out_str.flush();
             receiveFile();
-            in.close();
+            input.close();
         } catch (IOException e) {
-            System.out.println("Error: Server error. Connection closed.");
+            System.out.println("Connection closed due to server error");
         }
     }
 
     public void receiveFile() {
         try {
             int bytesRead;
-            DataInputStream serverData = new DataInputStream(srvr_sckt.getInputStream());
-            String fileName = serverData.readUTF();
-            OutputStream output = new FileOutputStream(fileName);
-            long size = serverData.readLong();
+            DataInputStream server_data = new DataInputStream(srvr_sckt.getInputStream());
+            OutputStream output = new FileOutputStream(server_data.readUTF());
+            long size = server_data.readLong();
             byte[] buffer = new byte[1024];
-            while (size > 0 && (bytesRead = serverData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+            while (size > 0 && (bytesRead = server_data.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
                 output.write(buffer, 0, bytesRead);
                 size -= bytesRead;
             }
             output.close();
-            serverData.close();
+            server_data.close();
         } catch (IOException e) {
-            System.out.println("Error: Server error. Connection closed.");
+            System.out.println("Connection closed due to server error");
         }
     }
 }
