c6904b3a112d977f23f7b1831c842949e30f664f
Uzama Zaid <uzamajaward@gmail.com>
2021-04-17 15:11:50 +0530

    starter initial commit

---
+++ Starter/MessageBroker.java
@@ -0,0 +1,47 @@
+package Starter;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class MessageBroker {
+    private final DatagramSocket datagramSocket;
+
+    public MessageBroker() throws SocketException {
+        this.datagramSocket = new DatagramSocket();
+    }
+
+    public MessageBroker(DatagramSocket datagramSocket) {
+        this.datagramSocket = datagramSocket;
+    }
+
+    public void send(DatagramPacket datagramPacket, int timeout) throws IOException {
+        datagramSocket.setSoTimeout(timeout);
+        datagramSocket.send(datagramPacket);
+    }
+
+    public DatagramPacket receive(int timeout) throws IOException {
+        byte[] buffer = new byte[65536];
+        DatagramPacket requestDatagramPacket = new DatagramPacket(buffer, buffer.length);
+        datagramSocket.setSoTimeout(timeout);
+        datagramSocket.receive(requestDatagramPacket);
+        return requestDatagramPacket;
+    }
+
+    public String sendAndReceive(String request, String ipAddress, int port, int timeout) throws IOException {
+        DatagramPacket requestDatagramPacket = new DatagramPacket(
+                request.getBytes(),
+                request.length(),
+                InetAddress.getByName(ipAddress),
+                port
+        );
+        datagramSocket.setSoTimeout(timeout);
+        datagramSocket.send(requestDatagramPacket);
+        byte[] buffer = new byte[65536];
+        DatagramPacket responseDatagramPacket = new DatagramPacket(buffer, buffer.length);
+        datagramSocket.receive(responseDatagramPacket);
+        return new String(responseDatagramPacket.getData(), 0, responseDatagramPacket.getLength());
+    }
+}
